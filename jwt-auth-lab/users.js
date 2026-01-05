28e2242cde74248d0270044203a07066bf4cfe0e
morshaline1504 <bsse1504@iit.du.ac.bd>
2026-01-05 19:32:04 +0600

    Update users.js

--- jwt-auth-lab/users.js
+++ jwt-auth-lab/users.js
@@ -4,13 +4,13 @@ const users = [
   {
     id: 1,
     username: "admin",
-    password: bcrypt.hasJhSync("admin123", 10),
+    password: bcrypt.hashSync("admin123", 10), // <-- hashSync, not hasJhSync
     role: "admin"
   },
   {
     id: 2,
     username: "student",
-    password: bcrypt.hashSync("student123", 10),
+    password: bcrypt.hashSync("student123", 10), // <-- hashSync
     role: "student"
   }
 ];
